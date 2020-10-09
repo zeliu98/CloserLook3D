376be09fba29d768c00a7939f0372d4056247e24
Zhijie Wang <paul.wangzhijie@outlook.com>
2020-10-09 01:38:38 -0400

    Fix complie error in pt_custom_ops (#20)
    Fix complie error in pt_custom_ops

--- pytorch/ops/pt_custom_ops/setup.py
+++ pytorch/ops/pt_custom_ops/setup.py
@@ -8,12 +8,14 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import glob
+import os.path as osp
 
 _ext_src_root = "_ext_src"
 _ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
     "{}/src/*.cu".format(_ext_src_root)
 )
 _ext_headers = glob.glob("{}/include/*".format(_ext_src_root))
+this_dir = osp.dirname(osp.abspath(__file__))
 
 setup(
     name='pt_custom_ops',
@@ -25,6 +27,7 @@ setup(
                 "cxx": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
                 "nvcc": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
             },
+            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
         )
     ],
     cmdclass={
